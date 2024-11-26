344b3c8643525240d427f024ce9084f78b2cb36d
DESKTOP-IJSG3N5\admin <zmddhkddl3211@naver.com>
2024-11-26 16:31:14 +0900

    java 형변환

---
+++ workspace_JAVA/chap02/src/sec01/exam02/TypeCastingExam.java
@@ -0,0 +1,51 @@
+package sec01.exam02;
+
+public class TypeCastingExam {
+
+	public static void main(String[] args) {
+
+		int a = 2 + (3 * 4); // 우선순위
+		long b = (long) a;   // 형변환연산자
+		long c = a;
+		
+		long d = 3000000000L;
+		int e = (int)d;
+		System.out.println("e : " + e);
+//		long 을 int로 변환시
+//		8byte중에서 상위 4byte만 남겨서
+//		예측하지못한값이 나올수있어서
+//		[생력불가능]
+		
+//		long f = 10;
+//		int ii = 10;
+//		long f = (long)ii;
+//		long f = ii;
+		
+		int g = 3;
+		long h = 4L;
+		long i = (long)g + h;
+		
+		long i2 = g + h;
+		
+		double j = 3.3;
+		double k = (double) g + j;
+		double k2 = g + j;
+		System.out.println("(double)g :" + (double)g);
+
+		int g2 = (int)j;
+		System.out.println("(int)j : " + (int)j);
+		
+		double total = 8.3;
+		int xx = (int)total;
+		int count = 4;
+		System.out.println((int)total/count);
+		
+		double totl = 8.213;
+		int cot = 4;
+		double d_result = totl / cot;
+		int i_result = (int)d_result;
+		System.out.println("예아:" + (int)i_result);
+//		double -> int 로 변환시 소숫자리 나라감 
+		
+	}
+}
--- workspace_JAVA/chap02/src/sec01/exam02/VarTypeExam.java
+++ workspace_JAVA/chap02/src/sec01/exam02/VarTypeExam.java
@@ -3,8 +3,40 @@ package sec01.exam02;
 public class VarTypeExam {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		byte b = 127;
+//		b = 128;
+		char c1 = 'A';
+		System.out.println("c1 :" + c1);
+		char c2 = 67;  //'C'
+		System.out.println("c2 :" + c2);
+		char c3 = 'a'; 
+		System.out.println("c3-c1  :" + (c3-c1));
+		
+		String s1 = "박혜성";
+		String s2 = "\"박혜성\"";
+		System.out.println("s1 : " + s1);
+		System.out.println("s2 : " + s2);
+		
+		String s3 = "번호 \t이름";
+		System.out.println(s3);
+		String s4 = "번호 2\t이름";
+		System.out.println(s4);
+		
+		String s5 = "\n홍 길 동 \n김 자 바";
+		System.out.println("s5 :" +s5);
+		System.out.println();
+		System.out.print("1");
+		System.out.print("2");
+		System.out.println("3");
+		
+		double d = 0.123456789123456789;
+		System.out.println("d : " + d);
+		float f = 0.123456789123456789f;
+		System.out.println("f : " + f);
+		
+		boolean b1 = true;
+		boolean b2 = false;
 	}
 
 }

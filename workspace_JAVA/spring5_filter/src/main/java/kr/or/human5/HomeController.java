ec345ea0a39143d31c332d257a64ebd829ebe030
DESKTOP-IJSG3N5\admin <zmddhkddl3211@naver.com>
2025-04-07 17:08:01 +0900

    JUnit & Log4j & filter

--- workspace_JAVA/spring5_filter/src/main/java/kr/or/human5/HomeController.java
+++ workspace_JAVA/spring5_filter/src/main/java/kr/or/human5/HomeController.java
@@ -1,4 +1,4 @@
-package kr.or.human;
+package kr.or.human5;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -25,10 +25,6 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		logger.error("error");
-		logger.warn("warn");
-		logger.debug("debug");
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
